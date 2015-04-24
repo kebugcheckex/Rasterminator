fad176a47cefe745443fb0e38eed1c5f2e52d2c0
Xinyu Chen <collectchen@gmail.com>
2015-04-24 10:56:49 -0700

    Added soild texture

--- RealWorld/main.cpp
+++ RealWorld/main.cpp
@@ -16,7 +16,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 int main(int argc, char *argv[])
 {
-	std::string texture_filename("grass.dds");
+	std::string texture_filename("soil.dds");
 	std::string object_filename("car.object");
 	if (argc > 3) texture_filename = argv[2];
 	if (argc > 2) object_filename = argv[1];
